356313d65f70924a8851f8455f29da9d268f6830
Ben <ben@insecure.co.za>
2012-09-04 16:10:15 -0700

    testing

--- app/conf/settings.php
+++ app/conf/settings.php
@@ -6,7 +6,7 @@ $siteconf=array(
     "requestcomponent"=>array('request'=> array('POST', 'GET', 'COOKIE', 'FILES', 'SERVER')),
     "sessioninfo"=>array(
     'session_name'=>'change_this_to_example_org',
-    'cookie_lifetime'=>86400, //max lifetime of a session between page clicks
+    'cookie_lifetime'=>86400, //Max lifetime of a session between page clicks
     'model'=>'mysql',
     'table_name'=>'php_session',
     'mysqlfile'=>'app/conf/sessiondb.php'
