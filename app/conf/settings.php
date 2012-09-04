75d56a2e3bca6fa28dd60364cf46d1618a072b08
Ben <ben@insecure.co.za>
2012-09-04 15:39:36 -0700

    added .gitignore as a test and updated app/conf/settings.php that should be commited now

--- app/conf/settings.php
+++ app/conf/settings.php
@@ -1,13 +1,12 @@
 <?
-    $siteconf=array(
+$siteconf=array(
     "maintenance"=>"/maintain.html",
     "logfile"=>"html/messages.log",
     "mysqlconf"=>"app/conf/sitedb.php",
     "requestcomponent"=>array('request'=> array('POST', 'GET', 'COOKIE', 'FILES', 'SERVER')),
     "sessioninfo"=>array(
-    'session_name'=>'your_session_name',
-    //max lifetime of a session between page clicks
-    'cookie_lifetime'=>86400,
+    'session_name'=>'change_this_to_example_org',
+    'cookie_lifetime'=>86400, //max lifetime of a session between page clicks
     'model'=>'mysql',
     'table_name'=>'php_session',
     'mysqlfile'=>'app/conf/sessiondb.php'
