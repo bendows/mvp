9970b27d1197eb65456a564b8559baf40295184d
Ben <ben@insecure.co.za>
2012-09-12 14:30:26 -0700

    updates, with new lib dir, this commit makes the lib repo dated and at EOL

---
+++ app/views/elements/footer.php
@@ -0,0 +1,3 @@
+<div id="footer">
+    <p style='text-align:center;'>This page uses the <a href="http://matthewjamestaylor.com/blog/ultimate-3-column-holy-grail-ems.htm">Ultimate 'Holy Grail' 3 column Liquid Layout</a> by <a href="http://matthewjamestaylor.com">Matthew James Taylor</a>. View more <a href="http://matthewjamestaylor.com/blog/-website-layouts">website layouts</a> and <a href="http://matthewjamestaylor.com/blog/-web-design">web design articles</a>.</p>
+</div>
--- app/views/layouts/default_layout.php
+++ app/views/layouts/default_layout.php
@@ -40,8 +40,6 @@
         </div>
     </div>
 </div>
-<div id="footer">
-    <p>This page uses the <a href="http://matthewjamestaylor.com/blog/ultimate-3-column-holy-grail-ems.htm">Ultimate 'Holy Grail' 3 column Liquid Layout</a> by <a href="http://matthewjamestaylor.com">Matthew James Taylor</a>. View more <a href="http://matthewjamestaylor.com/blog/-website-layouts">website layouts</a> and <a href="http://matthewjamestaylor.com/blog/-web-design">web design articles</a>.</p>
-</div>
+<?=$this->element('footer');?>
 </body>
 </html>
