407c0dca86ca56a1cdd4a5ad3058c551173b3d87
Ben <ben@insecure.co.za>
2012-10-29 12:50:32 -0700

    minor updates

--- app/conf/settings.php
+++ app/conf/settings.php
@@ -4,17 +4,17 @@
 //like this: settings::get('keyname')
 //ie. echo settings::get('mysqlconf') //echo's "app/conf/sitedb.php"
 $siteconf=array(
-    "onlinetime"=>"20 October 2012",
+    "onlinetime"=>"30 October 2012", //Time that the site will be online again
     //"maintenance"=>"/maintain.html", //If this line is NOT hashed out, the site will be operating in maintenance mode
     "logfile"=>"messages.log",  //Hash out this line to disable logging throughout the site
     "mysqlconf"=>"app/conf/sitedb.php",
     "requestcomponent"=>array('POST', 'GET', 'COOKIE', 'FILES', 'SERVER'),
     "sessioninfo"=>array(
-    'session_name'=>'change_this_to_example_org',
-    'cookie_lifetime'=>86400, //Max lifetime of a session between page clicks
-    'model'=>'mysql',
-    'table_name'=>'php_session',
-    'mysqlfile'=>'app/conf/sessiondb.php'
-)
+        'session_name'=>'change_this_to_example_org',
+        'cookie_lifetime'=>86400, //Max lifetime of a session between page clicks
+        'model'=>'mysql',
+        'table_name'=>'php_session',
+        'mysqlfile'=>'app/conf/sessiondb.php'
+    )
  );
 ?>
