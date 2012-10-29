811d3f4f212fc314a65ab5ab457e5f19b0694c45
Ben <ben@insecure.co.za>
2012-10-29 13:30:15 -0700

    minor updates

--- app/routers/default.php
+++ app/routers/default.php
@@ -5,15 +5,16 @@
 		public function getpagename () {
 			switch (true) {
 				case preg_match ("%^index.php$%", $this->url):
-        		case preg_match ("%^index.html$%", $this->url):
-        		case preg_match ("%^index$%", $this->url):
-          			$apage = "app_page_index";
-          			break;
-			default:
+				case preg_match ("%^index.html$%", $this->url):
+				case preg_match ("%^index$%", $this->url):
+					$apage = "app_page_index";
+					break;
+				default:
 					$apage = parent::getpagename();
 					break;
 			}
 			return $apage;
 		}
+
 	}
 ?>
