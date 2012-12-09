46f3d1b587067ff727659ff54d0c2f8c26ead814
root <root@apache1.theitnetwork.co.za>
2012-12-09 21:03:19 +0200

    updates

--- app/views/404.php
+++ app/views/404.php
@@ -2,9 +2,11 @@
 $url = preg_replace("/^./","", $this->here);
 ?>
 <h2>404 Page not found</h2>
-<p>No route found for The URL[<?=$this->here;?>]</p>
-<p>To redefine this view, edit app/pages/404.php</p>
-<p>To add a route for The URL[<?=$this->here;?>] add the folloing case statement inside the switch statement in the file app/routers/default.php</p>
+<p>To redefine this default error page edit app/pages/404.php</p>
+<p>To redefine this default error view edit app/views/404.php</p>
+
+<p>No route found for the URL[<?=$this->here;?>]</p>
+<p>To add a route for the URL[<?=$this->here;?>] add the folloing case statement inside the switch statement in the file app/routers/default.php</p>
 <pre>
 	case preg_match ("%^<?=$url;?>$%", $this->url):
 		$apage = "app_page_<?=$url;?>";
