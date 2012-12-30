adf5036a697fcfcb87ee4fd74d4fe6013ec33912
root <root@apache1.theitnetwork.co.za>
2012-12-30 02:35:20 +0200

    updates before login

--- app/pages/index.php
+++ app/pages/index.php
@@ -1,9 +1,8 @@
 <?
-class app_page_index extends app_page_app_page {
+class app_page_index extends app_page_app_session {
 	var $autorender = true;
 	var $viewfile = "index";
 	var $layout = "3col_layout";
 	var $title = "mvp successfully installed";
-	//var $components = array("request");
 }
 ?>
