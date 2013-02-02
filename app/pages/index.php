bff9246f067b498474a9a77c520a54d9b6908edf
root <root@apache1.theitnetwork.co.za>
2013-02-02 23:06:09 +0200

    testing, auth component next

--- app/pages/index.php
+++ app/pages/index.php
@@ -2,5 +2,17 @@
 class app_page_index extends app_page_app_page {
 	var $viewfile = "index";
 	var $title = "mvp successfully installed";
+
+	function initialize() {
+		parent::initialize();
+		if (! isset($_SESSION['started'])) {
+			$this->viewvars['setted']="Set";
+			$_SESSION['started'] = time();
+		}	else {
+			$this->viewvars['setted']="no";
+		}
+		$this->viewvars['started'] = $_SESSION['started'];
+		$this->viewvars['elapsed'] = time()-$_SESSION['started'];
+	}
 }
 ?>
--- app/views/index.php
+++ app/views/index.php
@@ -2,6 +2,8 @@
 <?$this->addscript("<script type='text/javascript' src='/js/captcha.js'></script>");?>
 
 <h2> mvp successfully installed on <?=$this->httphost;?></h2>
+<h2><?=$started;?></h2>
+<h2><?=$elapsed;?></h2>
 <?=$this->element('debug');?>
 <p>
 The left column and the right column are included inside the layout [<?=$this->layout;?>] as elements.<br>
